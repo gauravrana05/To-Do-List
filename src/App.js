4e5a0bc1c9773587ad7dbb4e728fcb9de7980424
gauravrana05 <ranaofficial05@gmail.com>
2023-07-15 18:54:58 +0530

    hstack

--- src/App.js
+++ src/App.js
@@ -1,57 +1,89 @@
-import {  VStack,Text } from "@chakra-ui/react"
-import './App.css';
-import {useState} from 'react'
-import AddTodo from "./components/AddTodo";
-import TodoList from "./components/TodoList";
+import { Stack, HStack, VStack, Text } from "@chakra-ui/react"
+import "./App.css"
+import { useState } from "react"
+import AddTodo from "./components/AddTodo"
+import TodoList from "./components/TodoList"
 
 function App() {
- 
-const todosList = [
-  { id: 1, text: 'Buy eggs'},
-  { id: 2, text: 'Walk the dog'},
-  { id:3, text: 'Watch a movie'}
-];
+  const todosList = [
+    { id: 1, text: "Buy eggs" },
+    { id: 2, text: "Walk the dog" },
+    { id: 3, text: "Watch a movie" },
+  ]
 
-const [todos, setTodos] = useState(todosList);
+  const taskList = [
+    { id: 1, task: { title: "", description: "" }, type: "notStarted" },
+    { id: 2, task: { title: "", description: "" }, type: "notStarted" },
+    { id: 3, task: { title: "", description: "" }, type: "notStarted" },
+  ]
 
+  const [todos, setTodos] = useState(todosList)
 
-function deleteTodo(id){
-const newTodos = todos.filter((item)=> {
-  return item.id !== id 
-})
-setTodos(newTodos)
-console.log(newTodos)
-}
+  const [tasks, setTasks] = useState(taskList)
 
-function addTodo(newTodo){
-setTodos([...todos,newTodo])
-}
+  function deleteTodo(id) {
+    const newTodos = todos.filter((item) => {
+      return item.id !== id
+    })
 
-function editTodo(id,updatedTodo){
-const updatedItem = todos.map((todo) => {
-    return todo.id === id ? updatedTodo : todo;
-  });
-setTodos(updatedItem)
-}
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
 
+  function addTodo(newTodo) {
+    setTodos([...todos, newTodo])
+  }
 
+  function editTodo(id, updatedTodo) {
+    const updatedItem = todos.map((todo) => {
+      return todo.id === id ? updatedTodo : todo
+    })
+    setTodos(updatedItem)
+  }
 
   return (
-    <VStack p={5}>
-    
-    <Text bgGradient="linear(to-l, #7928CA,#FF0080)"
-      bgClip="text"
-      fontSize="6xl"
-      fontWeight="extrabold">
-      Todo App
-    </Text>
-       
-    <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo}/>
-    <AddTodo addTodo={addTodo}/>
+    <HStack>
+      <VStack p={5}>
+        <Text
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+        >
+          Todo App
+        </Text>
 
+        <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
+        <AddTodo addTodo={addTodo} />
+      </VStack>
+      <VStack p={5}>
+        <Text
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+        >
+          Todo App
+        </Text>
 
-    </VStack>
-  );
+        <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
+        <AddTodo addTodo={addTodo} />
+      </VStack>
+      <VStack p={5}>
+        <Text
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+        >
+          Todo App
+        </Text>
+
+        <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
+        <AddTodo addTodo={addTodo} />
+      </VStack>
+    </HStack>
+  )
 }
 
-export default App;
+export default App
